ca0d60b9ce5b71f9c25d0ac32cb82b45d5b6597e
gersi <gersi@DESKTOP-QC9A9Q8>
2021-08-30 19:29:02 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioDSD/AnimaisResgatados.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Serialization;
+
+namespace ExercicioDSD
+{
+    [DataContract]
+    public class AnimaisResgatados
+    {      
+
+        [DataMember(Name = "Nome")]
+        public string Nome { get; set; }
+
+        [DataMember(Name = "TipoDeAnimal")]
+        public string TipoDeAnimal { get; set; }
+
+        [DataMember(Name = "AustrPaisOrigemalia")]
+        public string PaisOrigem { get; set; }
+
+        [DataMember(Name = "QuantidadeEncontrada")]
+        public int QuantidadeEncontrada { get; set; }
+    }
+}
---
+++ ExercicioDSD/Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace ExercicioDSD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AnimaisResgatados Object = new AnimaisResgatados()
+            {
+                Nome = "Canguru",
+                TipoDeAnimal = "Marsupial",
+                PaisOrigem = "Australia",
+                QuantidadeEncontrada = 30
+            };
+
+            string creditApplicationJson = JsonConvert.SerializeObject(
+    new
+    {
+        jsonCreditApplication = Object
+    });
+            Console.WriteLine(creditApplicationJson);
+
+        }
+    }
+}
